6e9ae398d727d028fd61aaa9f8b6206b778ddb3b
Michael Withington <withingtonm@wit.edu>
2017-01-11 13:07:49 -0500

    This is our skeletal commit, basic node server no mongo integrated.

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express'),
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
+
+//var mongodb = require('mongodb'); 
+
+
+server.listen(3000);
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(cookieParser());
+
+app.use(express.static(__dirname +  '/frontend/app/'));
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = app;
