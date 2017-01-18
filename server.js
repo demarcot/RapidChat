b131128ae50b98f4a666e1f423df7dc3d35527a7
demarcot <demarcot@wit.edu>
2017-01-18 12:47:23 -0500

    Basic MongoDB connectivity

--- server.js
+++ server.js
@@ -6,8 +6,22 @@ var express = require('express'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser');
 
-//var mongodb = require('mongodb');
-
+var mongodb = require('mongodb');
+var runningDB;
+var MongoClient = mongodb.MongoClient;
+var myUrl = 'mongodb://localhost:27017';
+MongoClient.connect(myUrl, function(err, db) {
+  
+  if(err)
+  {
+    console.log("Error connecting to MongoDB... you suck!");
+  }
+  else
+  {
+    console.log("Connection established to: ", myUrl);
+    runningDB = db;
+  }
+});
 
 server.listen(3000);
 app.use(bodyParser.urlencoded());
