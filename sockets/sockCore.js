dd7c36be6612c2bbbd24bd36b991352c352915b7
Michael Withington <withingtonm@wit.edu>
2017-04-24 17:03:11 -0400

    mike - Video chat test works!!!!!

--- sockets/sockCore.js
+++ sockets/sockCore.js
@@ -59,55 +59,56 @@ module.exports = function (io) {
     // NOTE: this is shit i dont want in the final verison
 
     // convenience function to log server messages on the client
-  // function log() {
-  //   var array = ['Message from server:'];
-  //   array.push.apply(array, arguments);
-  //   socket.emit('log', array);
-  // }
-  //
-  // socket.on('messageRTC', function(message) {
-  //   log('Client said: ', message);
-  //   // for a real app, would be room-only (not broadcast)
-  //   socket.broadcast.emit('messageRTC', message);
-  // });
-  //
-  // socket.on('create or join', function(room) {
-  //   log('Received request to create or join room ' + room);
-  //
-  //   var numClients = io.sockets.sockets.length;
-  //   log('Room ' + room + ' now has ' + numClients + ' client(s)');
-  //
-  //   if (numClients === 1) {
-  //     socket.join(room);
-  //     log('Client ID ' + socket.id + ' created room ' + room);
-  //     socket.emit('created', room, socket.id);
-  //
-  //   } else if (numClients === 2) {
-  //     log('Client ID ' + socket.id + ' joined room ' + room);
-  //     io.sockets.in(room).emit('join', room);
-  //     socket.join(room);
-  //     socket.emit('joined', room, socket.id);
-  //     io.sockets.in(room).emit('ready');
-  //   } else { // max two clients
-  //     socket.emit('full', room);
-  //   }
-  // });
-  //
-  // socket.on('ipaddr', function() {
-  //   var ifaces = os.networkInterfaces();
-  //   for (var dev in ifaces) {
-  //     ifaces[dev].forEach(function(details) {
-  //       if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-  //         socket.emit('ipaddr', details.address);
-  //       }
-  //     });
-  //   }
-  // });
-  //
-  // socket.on('bye', function(){
-  //   console.log('received bye');
-  // });
-  //
+  function log() {
+    var array = ['Message from server:'];
+    array.push.apply(array, arguments);
+    socket.emit('log', array);
+  }
+
+  socket.on('messageRTC', function(message) {
+    log('Client said: ', message);
+    // for a real app, would be room-only (not broadcast)
+    socket.broadcast.emit('messageRTC', message);
+  });
+
+  socket.on('create or join', function(room) {
+    log('Received request to create or join room ' + room);
+
+    var numClients = io.sockets.clients().length;
+    console.log(numClients);
+    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+    if (numClients === 1) {
+      socket.join(room);
+      log('Client ID ' + socket.id + ' created room ' + room);
+      socket.emit('created', room, socket.id);
+
+    } else if (numClients === 2) {
+      log('Client ID ' + socket.id + ' joined room ' + room);
+      io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room, socket.id);
+      io.sockets.in(room).emit('ready');
+    } else { // max two clients
+      socket.emit('full', room);
+    }
+  });
+
+  socket.on('ipaddr', function() {
+    var ifaces = os.networkInterfaces();
+    for (var dev in ifaces) {
+      ifaces[dev].forEach(function(details) {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+          socket.emit('ipaddr', details.address);
+        }
+      });
+    }
+  });
+
+  socket.on('bye', function(){
+    console.log('received bye');
+  });
+
 
 
    });
