dbf8f87e0f611d0fc5a869457dc7236ceca087c6
Michael Withington <withingtonm@wit.edu>
2017-05-14 12:38:29 -0400

    Mike- A new forged friendship between U and I

--- frontend/app/assets/scripts/controllers/demoCtrl.js
+++ frontend/app/assets/scripts/controllers/demoCtrl.js
@@ -71,16 +71,6 @@
         });
 
     };
-  })
-    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      // Component lookup should always be available since we are not using `ng-if`
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
-
-    };
   }).controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
     $scope.close = function () {
       // Component lookup should always be available since we are not using `ng-if`
