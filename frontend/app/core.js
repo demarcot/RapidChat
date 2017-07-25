8d0d65316e55cf4359fb8b4f1e439c4859850838
Michael Withington <mwithington@gemvara.com>
2017-07-25 12:14:04 -0400

    Mike - linkify integrated for file transfer and outbound links

--- frontend/app/core.js
+++ frontend/app/core.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('coreApp', ['ui.router', 'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'luegg.directives', 'ui.identicon', 'ngMaterial', 'material.svgAssetsCache'])
+        .module('coreApp', ['ui.router', 'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'luegg.directives', 'ui.identicon', 'ngMaterial', 'material.svgAssetsCache','linkify'])
         .value('nickName', 'Nick_name')
         .config(config)
         .run(run);
